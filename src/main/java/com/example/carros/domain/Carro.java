67c1354244060b24ad9016c690d87433590c0e1a
Carlos Martins <lemos_oliveiraa@hotmail.com>
2023-07-20 18:18:06 -0300

    [feature] Instalação e utilização Lombok

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Carro {
 
     @Id
@@ -14,32 +16,6 @@ public class Carro {
     private String nome;
 
     private String tipo;
-
-    public Carro(Long id, String nome, String tipo) {
-        this.id = id;
-        this.nome = nome;
-        this.tipo = tipo;
-    }
-
-    public Carro() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public String getTipo() {
         return tipo;
